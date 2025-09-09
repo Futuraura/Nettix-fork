29e47154a58177637cdf99c7a5a69679ed1144bf
Eino627 <el0772@edu.turku.fi>
2025-09-09 08:57:16 +0300

    Kuvien sijoitus listasta

---
+++ Tuotteet/Tuote.js
@@ -0,0 +1,16 @@
+
+const Kuvat = [
+    "Kuvat/Kuva1.jpg"
+];
+
+
+let tuote_kuva1 = document.getElementById("ostos_kuva1");
+let tuote_kuva2 = document.getElementById("ostos_kuva2");
+let tuote_kuva3 = document.getElementById("ostos_kuva3");
+tuote_kuva1.innerHTML = `<img src="${Kuvat[0]}">`;
+tuote_kuva2.innerHTML = `<img src="${Kuvat[0]}">`;
+tuote_kuva3.innerHTML = `<img src="${Kuvat[0]}">`;
+
+
+
+
