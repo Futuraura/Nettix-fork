94afdda34e901052851d8dcd52742fd94c4ca6b7
Eino627 <el0772@edu.turku.fi>
2025-09-16 11:52:05 +0300

    Js ostoskori

--- Tuotteet/Tuote.js
+++ Tuotteet/Tuote.js
@@ -12,5 +12,15 @@ tuote_kuva2.innerHTML = `<img src="${Kuvat[0]}">`;
 tuote_kuva3.innerHTML = `<img src="${Kuvat[0]}">`;
 
 
-
+// Get info from a form with id "myForm"
+const form = document.getElementById("ostoskori_tieto");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+    const formData = new FormData(form);
+    const väri1 = formData.get("thing1");
+    const väri2 = formData.get("thing2");
+    const väri3 = formData.get("thing3");
+    
+    console.log("Väri1:", väri1);
+});
 
