a15c473f1db7e3465acc6f3022d5c51792596108
Vipeaja <aatos.koskelainen@gmail.com>
2025-09-19 12:11:00 +0300

    template

--- tuotelistid.js
+++ tuotelistid.js
@@ -5,6 +5,7 @@ var tuotelistaid = [{
         hinta:"10",
         kuva:"Tuotteet/Kuvat/kuva1.jpg"
     },
+    //---temps---
     { 
         tuotenumero:"2",
         nimi:"housu",
@@ -17,7 +18,16 @@ var tuotelistaid = [{
         hinta:"41",
         kuva:"Tuotteet/Kuvat/kuva1.jpg"
     }
+    //------------
 ]
 
 localStorage.setItem("listaidref", JSON.stringify(tuotelistaid));
 
+//-----template------
+
+//    { 
+//        tuotenumero:"2",
+//        nimi:"housu",
+//        hinta:"40",
+//        kuva:"Tuotteet/Kuvat/kuva1.jpg"
+//    }
