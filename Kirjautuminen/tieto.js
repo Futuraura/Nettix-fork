31b076093b7e079a0ebeefe64135b6c0d24ab2fe
Eino627 <el0772@edu.turku.fi>
2025-09-26 11:51:34 +0300

    Kirjautumis js

--- Kirjautuminen/tieto.js
+++ Kirjautuminen/tieto.js
@@ -1,1 +1,37 @@
-function getData() {}
+function getData(event) {
+  event.preventDefault();
+  const email = document.getElementById("R_email").value;
+  const NewPassword = document.getElementById("R_password").value;
+  const confirmPassword = document.getElementById("confirmPassword").value;
+  const tarjous = document.getElementById("tarjous").checked;
+  if (!email || !NewPassword || !confirmPassword) {
+    alert("Täytä kaikki kentät!");
+    return;
+  }
+  if (NewPassword !== confirmPassword) {
+    alert("Salasanat eivät täsmää!");
+    return;
+  }
+  if (NewPassword.length < 6) {
+    alert("Salasanan tulee olla vähintään 6 merkkiä pitkä!");
+    return;
+  } else {
+    UserData = {
+      email: email,
+      Password: NewPassword,
+      tarjous: tarjous,
+    };
+    window.location.href = "../index.html";
+  }
+}
+function SignIn(event) {
+  event.preventDefault();
+  const email = document.getElementById("K_email").value;
+  const password = document.getElementById("K_password").value;
+  if (!email || !password) {
+    alert("Täytä kaikki kentät!");
+    return;
+  }
+  window.location.href = "../index.html";
+}
+//kesken
